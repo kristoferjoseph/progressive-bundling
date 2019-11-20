b4e1e2df1dd8c1a166dc4123b1632c5191542247
kj <kristoferjoseph@gmail.com>
2019-11-20 14:44:01 -0800

    Initial project structure.

--- src/http/get-js-000type-000module/index.js
+++ src/http/get-js-000type-000module/index.js
@@ -1,38 +1,30 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+const { promisify } = require('util')
+const readFile = promisify(require('fs').readFile)
+const join = require('path').join
 
-let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
-  </head>
-  <body>
-    <h1 class="center-text">
-      Hello world!
-    </h1>
-    <p class="center-text">
-      Your new route is ready to go!
-    </p>
-    <p class="center-text">
-      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
-    </p>
-  </body>
-</html>
-`
-
-// HTTP function
 exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    headers: {
-      'content-type': 'text/html; charset=utf8'
-    },
-    body: html
+  let params = req.params || {}
+  let type = params.type
+  let module = params.module
+  let response = {
+    status: 404,
+    body: 'File not found'
   }
+  if (type && module) {
+    try {
+      response = {
+        headers: {
+          'content-type': 'application/javascript; charset=utf8'
+        },
+        body: fs.readFile(join(__dirname, 'node_modules', '@architect', 'views', type, module))
+      }
+    } catch (err) {
+      response = {
+        status: 500,
+        body: err
+      }
+    }
+  }
+
+  return response
 }
---
+++ src/views/ui/header.js
@@ -0,0 +1,6 @@
+import { html } from ''
+export default function Header (props) {
+  return html`
+<header ...${props}></header>
+  `
+}
